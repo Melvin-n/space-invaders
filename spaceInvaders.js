b1a161dc3f1aca63051914b0c4bc1a6beb60897d
melvin <melvinn721@gmail.com>
2021-10-22 23:15:15 +1300

    added movement function, started shooting function but needs fixing

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -1,29 +1,21 @@
-//define the users color choice
-let color;
 
-//define canvas/gamescreen
-const gameScreen = document.querySelector('#gameScreen')
-const colors = document.querySelector('#colors')
+//define players constants
+let playerx = 250;
+let playery = 295;
 
 
-//upon color selection, save the color and change screen to game canvas
-colors.addEventListener('click', (e) => {
-    if (e.target.id != 'colors') {
-    color = e.target.id
-    console.log(color)
-    gameScreen.innerHTML = `<canvas id='canvas' width='500' height='350'>Pong?</canvas>`
-    startGame();
-    }
-})
+
 //define start game function, draws the players ship on screen, defines the canvas\
 //have to do in seperate function otherwise it initiates before color is set from addeventlistener
 //called inside eventlistener after color is selected
+
 
 function startGame(){
     //define canvas
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d');
-
+    //clear the rect each time function is loaded (each interval)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     //define player ship class
     class Ship {
         constructor(x, y, color) {
@@ -31,7 +23,8 @@ function startGame(){
             this.x = x;
             this.color = color
         }
-        draw(context){
+        //draw the ship
+        draw(ctx){
             ctx.beginPath()
             ctx.moveTo(this.x, this.y)
             ctx.lineTo(this.x + 10, this.y + 7.5)
@@ -45,13 +38,84 @@ function startGame(){
             ctx.lineTo(this.x - 25, this.y + 38.5)
             ctx.lineTo(this.x - 10, this.y + 28.5)
             ctx.lineTo(this.x - 10, this.y + 7.5)
-            // ctx.lineTo(this.x - 20, this.y - 15)
             ctx.lineTo(this.x, this.y)
             ctx.fillStyle = color
             ctx.fill();
+            ctx.closePath()
+        }
+    }
+    let playerShip = new Ship(playerx, playery, color);
+    playerShip.draw(ctx)
+    
+   
+    document.addEventListener('keyown', playerShoot) 
+    function playerShoot(e) {
+        console.log(e)    
+        if (e.key == ' ') {
+            shootBullet(ctx)
         }
     }
 
-    let playerShip = new Ship(250, 250, color);
-    playerShip.draw(ctx)
 }
+//define bullet speed
+const bulletVelocity = 20;
+const bulletStartY = playery - 38.5
+const bulletStartX = playerx 
+const bulletWidth = 3
+const bulletHeight = 6
+//call function upon spacebar press
+
+
+
+//shoot function
+function shootBullet(ctx) {
+    class Bullet {
+        constructor(x, y, color) {
+            x = this.x
+            y = this.y
+            color = this.color
+        }
+    
+        draw(ctx){
+            ctx.beginPath()
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(this.x + bulletWidth, this.y)
+            ctx.lineTo(this.x + bulletWidth, this.y + bulletHeight)
+            ctx.lineTo(this.x, this.y + bulletHeight)
+            ctx.lineTo(this.x, this.y)
+            ctx.fillStyle = this.color
+            ctx.fill()
+            ctx.closePath()
+        }
+    }
+
+    let playerBullet = new Bullet(bulletStartX,bulletStartY, color) 
+    playerBullet.draw(ctx)
+    
+}
+
+    
+
+
+//define players movement with arrow keys
+const speed = 10;
+document.addEventListener('keydown', playerMovement)
+
+function playerMovement(e) { 
+        if (e.key == 'ArrowLeft' && playerx >= 25) {
+            playerx -= speed;
+            console.log(playerx)
+            
+        } else if (e.key == 'ArrowRight' && playerx <= 475) {
+            playerx += speed;
+            console.log(playerx)
+        }
+}
+
+//player shoot function
+
+
+
+
+
+
